a956826aa1f852d63dfe9b820b68c102efbc3748
josehung17 <joseantoniohung@gmail.com>
2025-07-08 17:10:39 -0400

    creando el menu responsive

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,21 +16,27 @@
                         Dashboard
                     </x-nav-link>
                 </div>
+                @can('view marcas')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('marcas.index')" :active="request()->routeIs('marcas.index')">
                         Marcas
                     </x-nav-link>
                 </div>
+                @endcan
+                @can('view categories')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('categorias.index')" :active="request()->routeIs('categorias.index')">
                         Categorias
                     </x-nav-link>
                 </div>
+                @endcan
+                @can('view products')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('productos.index')" :active="request()->routeIs('productos.index')">
                         Productos
                     </x-nav-link>
                 </div>
+                @endcan
                 @can('view users')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
@@ -92,6 +98,26 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            @can('view marcas')
+            <x-responsive-nav-link :href="route('marcas.index')" :active="request()->routeIs('marcas.index')">
+                Marcas
+            </x-responsive-nav-link>
+            @endcan
+            @can('view categories')
+            <x-responsive-nav-link :href="route('categorias.index')" :active="request()->routeIs('categorias.index')">
+                Categorias
+            </x-responsive-nav-link>
+            @endcan
+            @can('view products')
+            <x-responsive-nav-link :href="route('productos.index')" :active="request()->routeIs('productos.index')">
+                Productos
+            </x-responsive-nav-link>
+            @endcan
+            @can('view users')
+            <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                Usuarios
+            </x-responsive-nav-link>
+            @endcan
         </div>
 
         <!-- Responsive Settings Options -->
